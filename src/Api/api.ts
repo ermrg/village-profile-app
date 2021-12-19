7fa51c89b863709ccf9b60445e0cb3eca015b252
Madhuraman Ghimire <madhuramanghimire@gmail.com>
2021-12-19 14:32:57 +0545

    Add .env.example

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { IUser } from "../db/models/UserModel";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
-const server = `${process.env.REACT_APP_SERVER}/api/`;
+const server = `${process.env.REACT_APP_SERVER ? process.env.REACT_APP_SERVER : 'https://8844-2400-1a00-b050-148b-90ff-9710-9087-6c43.ngrok.io'}/api/`;
 const api = {
   loadCollectors: () => axios.get(`${server}collectors/`),
   loadWada: (office_id: String) => axios.get(`${server}wards/`, {params: {office_id: office_id}}),
